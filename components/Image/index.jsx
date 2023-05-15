17a106e7880335b52b37e29c63d710492a32fbec
Tori Luszczek <wuszczek@gmail.com>
2023-05-15 14:50:03 +0200

    trying to add animmmmmmmmmmmmation. does not work yet

--- components/Image/index.jsx
+++ components/Image/index.jsx
@@ -1,5 +1,24 @@
 import Image from "next/image";
+import useState from "react";
 
 export default function PetImage() {
-  return <Image src="/gible.png" width={250} height={250} alt="pic" />;
+  function imageClick() {}
+
+  const [effect, setEffect] = useState(false);
+
+  return (
+    <Image
+      src="/gible.png"
+      width={250}
+      height={250}
+      alt="pic"
+      className={`${
+        effect && "animate-wiggle"
+      } bg-blue-500 p-4 text-white rounded hover:bg-blue-700 hover:shadow-xl`}
+      onClick={() => {
+        setEffect(true);
+      }}
+      onAnimationEnd={() => setEffect(false)}
+    />
+  );
 }
