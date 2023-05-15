9524175e4ebbef602f4f255beaa97aa4cc604c4b
Tori Luszczek <wuszczek@gmail.com>
2023-05-15 11:21:24 +0200

    sorting the pictures and first functions. just simple things for now

---
+++ components/Image/index.jsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function PetImage() {
+  return <Image src="/gible.png" width={250} height={250} alt="pic" />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Button from "./../../components/Button";
+import PetImage from "./../../components/Image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <div>
-          <h2>THIS IS A HEADER I THINK</h2>
+          <h2>Gible says be brave</h2>
         </div>
+        <PetImage />
         <Button />
       </div>
 
