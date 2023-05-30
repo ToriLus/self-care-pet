8fd64d6f50d72512089a99affb2593b603b843eb
Wiktoria Luszczek <wuszczek@gmail.com>
2023-05-30 23:31:09 +0200

    there should be more commits. on the other hand, this thing has now like 40 images and i think it might explode at some point. would be great to upload those to mondo or something.

--- components/Convo/index.jsx
+++ components/Convo/index.jsx
@@ -7,39 +7,69 @@ export default function Canvas() {
 
   const [color, setColor] = useState("#000");
 
+  const imagesArray = [
+    "/vectorised/tail_base.svg",
+
+    "/vectorised/tail_stripes.svg",
+
+    "/vectorised/left_foot.svg",
+    "/vectorised/right_foot.svg",
+    "/vectorised/ears_half_white.svg",
+    "/vectorised/ears_half_brown.svg",
+
+    "/separate_layers/ears_half_outline.png",
+    "/separate_layers/ears_half_shadow.png",
+
+    "/separate_layers/left_foot_outline.png",
+    "/separate_layers/left_foot_shadow.png",
+
+    "/separate_layers/right_foot_outline.png",
+    "/separate_layers/right_foot_shadow.png",
+    "/separate_layers/tail_outline.png",
+    "/separate_layers/tail_shadow.png",
+    //right hand
+
+    "/vectorised/right_hand.svg",
+    "/vectorised/right_paw.svg",
+    "/separate_layers/right_hand_shadow.png",
+    "/separate_layers/right_hand_outline.png",
+    //left hand
+
+    "/vectorised/left_hand.svg",
+    "/vectorised/left_paw.svg",
+
+    "/separate_layers/left_hand_shadow.png",
+    "/separate_layers/left_hand_outline.png",
+    //torso
+    "/vectorised/torso_col.svg",
+    "/vectorised/torso_white.svg",
+    "/separate_layers/torso_shadow.png",
+    "/separate_layers/torso_outline.png",
+
+    //head and mouth
+    "/vectorised/head_col.svg",
+    "/vectorised/head_white.svg",
+    "/separate_layers/head_outline.png",
+    "/separate_layers/head_shadow.png",
+    "/separate_layers/mouth.png",
+    "/separate_layers/nose.png",
+    "/separate_layers/eyes.png",
+  ];
+
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-
     // Load the image
-    const image = new Image();
-    image.src = "/separate_layers/rp_torso.png";
-    const secondImage = new Image();
-    secondImage.src = "/separate_layers/rp_tail.png";
 
-    // Draw the image on the canvas once it's loaded
-    image.onload = () => {
-      const canvasWidth = canvas.width;
-      const canvasHeight = canvas.height;
-
-      // Calculate the desired size of the image inside the canvas
-      const desiredWidth = canvasWidth * 0.8;
-      const desiredHeight = (desiredWidth / image.width) * image.height;
-
-      // Draw the image on the canvas with the desired dimensions
-      context.drawImage(secondImage, 0, 0, desiredWidth, desiredHeight);
-      context.drawImage(image, 0, 0, desiredWidth, desiredHeight);
-    };
-
-    image.onerror = () => {
-      console.error("Failed to load the image.");
-    };
-  }, []);
+    imagesArray.map((source) => {
+      const image = new Image();
+      image.src = source;
+    });
+  });
 
   return (
-    <div>
-      <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
-      <canvas ref={canvasRef} height={"600px"} width={"600px"} />
+    <div className="image-container">
+      {imagesArray.map((source) => {
+        return <img src={source} className="animalParts" />;
+      })}
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import styles from "./page.module.css";
-import Button from "./../../components/Button";
-import PetImage from "./../../components/Image";
-import Background from "./../../components/Background";
-import Stats from "./../../components/Stats/stats";
 import Canvas from "./../../components/Convo";
+import Button from "../../components/Button";
 
 export default function Home() {
   return (
@@ -12,8 +9,6 @@ export default function Home() {
         <div className={styles.description}>
           <Canvas />
         </div>
-
-        <div className={styles.center}></div>
       </main>
     </body>
   );
